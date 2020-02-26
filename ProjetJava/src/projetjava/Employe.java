16f36547b2c8cceb1795231379e8c3e871f497b2
JulesISIS <julesc@sfr.fr>
2020-02-26 14:27:07 +0100

    J'ai créé les classes, le constructeur et les Getter d'Employé

---
+++ ProjetJava/src/projetjava/Employe.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetjava;
+
+/**
+ *
+ * @author mjule
+ */
+public abstract class Employe {
+    private final String nom;
+    private final String prenom;
+    private final int matricule;
+
+    public Employe(String nom, String prenom, int matricule){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public int getMatricule() {
+        return matricule;
+    }
+    
+    public abstract float calculerSalaire();
+    
+}
