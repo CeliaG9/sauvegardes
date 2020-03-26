8473f3fd379ea9d0564015b67fcf0aa6aeda3a05
CéliaG9 <celiagalibert9@gmail.com>
2020-03-26 12:43:19 +0100

    Modifications

--- ProjetJava/src/projetjava/EmpBase.java
+++ ProjetJava/src/projetjava/EmpBase.java
@@ -9,13 +9,15 @@ package projetjava;
  *
  * @author mjule
  */
-public abstract class EmpBase extends Employe {
+public class EmpBase extends Employe {
     
     public EmpBase(String nom, String prenom, int matricule, int indice) throws EmployeException {
         super(nom, prenom, matricule, indice);
     }
     
-    public double calculSalaire() {
+    // Méthode permettant de calculer le salaire d'un employé de base 
+    @Override
+    public double calculerSalaire() {
         double salaire = 12*indice;
         return salaire;
     }
