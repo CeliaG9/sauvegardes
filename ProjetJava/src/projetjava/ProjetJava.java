88d7e5433c6348e686aad57563ab6727eaa50ae7
CéliaG9 <celiagalibert9@gmail.com>
2020-04-11 17:45:55 +0200

    remplissage de la hashSet() de subordonnés pour chaque Responsable remplissage de la hashSet() des employés de l'entrerprise Création d'une hiérarchie directe

--- ProjetJava/src/projetjava/ProjetJava.java
+++ ProjetJava/src/projetjava/ProjetJava.java
@@ -17,24 +17,72 @@ public class ProjetJava {
      */
     public static void main(String[] args) throws EmployeException {
         try {
+            
             // Création de l'entreprise
             Entreprise entreprise = new Entreprise();
 
-            // Création des employés : une dizaine d'employés au départ
-            // Responsables
-            entreprise.ajouterEmploye(new Responsable("HUBERT", "Lucas", 101, 175));
-            entreprise.ajouterEmploye(new Responsable("LAPORTE", "Anaïs", 102, 175));
-            // Employés de base
-            entreprise.ajouterEmploye(new EmpBase("GOMEZ", "Dominique", 201, 125));
-            entreprise.ajouterEmploye(new EmpBase("SIMON", "Claire", 202, 125));
-            entreprise.ajouterEmploye(new EmpBase("JOUBERT", "Marc", 203, 125));
-            entreprise.ajouterEmploye(new EmpBase("LEFEVRE", "Gabrielle", 204, 125));
-            entreprise.ajouterEmploye(new EmpBase("PETITJEAN", "Céline", 205, 125));
-            // Commerciaux
-            entreprise.ajouterEmploye(new Commercial("BESSON", "Olivier", 301, 150, 14));
-            entreprise.ajouterEmploye(new Commercial("GROS", "Paul", 302, 150, 18));
+            // Création des employés : une quinzaine d'employés au départ
+            // Création des responsables
+            Responsable resp1 = new Responsable("HUBERT", "Lucas", 101, 190);
+            Responsable resp2 = new Responsable("LAPORTE", "Anaïs", 102, 175);
+            Responsable resp3 = new Responsable("RIBEIRO", "Auguste", 103, 175);
+            Responsable resp4 = new Responsable("TURPIN", "Stéphanie", 104, 175);
+            Responsable resp5 = new Responsable("LEVEQUE", "Bertrand", 105, 175);
 
+            // Création des employés de base
+            EmpBase EB1 = new EmpBase("GOMEZ", "Dominique", 201, 125);
+            EmpBase EB2 = new EmpBase("SIMON", "Claire", 202, 125);
+            EmpBase EB3 = new EmpBase("JOUBERT", "Marc", 203, 125);
+            EmpBase EB4 = new EmpBase("LEFEVRE", "Gabrielle", 204, 125);
+            EmpBase EB5 = new EmpBase("PETITJEAN", "Céline", 205, 125);
+            EmpBase EB6 = new EmpBase("BLANCHARD", "Nathalie", 206, 125);
+            EmpBase EB7 = new EmpBase("RAYMOND", "Jacques", 206, 125);
+            
+            // Création des commerciaux
+            Commercial com1 = new Commercial("BESSON", "Olivier", 301, 150, 14);
+            Commercial com2 = new Commercial("GROS", "Paul", 302, 150, 18);
+            Commercial com3 = new Commercial("DURAND", "Marianne", 303, 150, 16);
+            
+            
             // Affectation de subordonnés à un responsable
+            // Pour cela, réalisation d'une hiérarchie sur papier
+            // Affectation de subordonnés à Lucas Hubert (resp1)
+            resp1.ajoutersubordonnes(resp2);
+            resp1.ajoutersubordonnes(resp3);
+            resp1.ajoutersubordonnes(EB1);
+            resp1.ajoutersubordonnes(EB2);
+            // Affectation de subordonnés à Anaïs Laporte (resp2)
+            resp2.ajoutersubordonnes(EB3);
+            resp2.ajoutersubordonnes(resp4);
+            // Affectation de subordonnés à Auguste Ribeiro (resp3)
+            resp3.ajoutersubordonnes(resp5);
+            // Affectation de subordonnés à Stéphanie Turpin (resp4)
+            resp4.ajoutersubordonnes(EB4);
+            resp4.ajoutersubordonnes(EB5);
+            resp4.ajoutersubordonnes(EB6);
+            // Affectation de subordonnés à Bertrand Leveque (resp 5)
+            resp5.ajoutersubordonnes(EB7);
+            
+            // Ajout des employés dans l'entreprise
+            // Ajout des responsables 
+            entreprise.ajouterEmploye(resp1);
+            entreprise.ajouterEmploye(resp2);
+            entreprise.ajouterEmploye(resp3);
+            entreprise.ajouterEmploye(resp4);
+            entreprise.ajouterEmploye(resp5);
+            // Ajout des employés de base
+            entreprise.ajouterEmploye(EB1);
+            entreprise.ajouterEmploye(EB2);
+            entreprise.ajouterEmploye(EB3);
+            entreprise.ajouterEmploye(EB4);
+            entreprise.ajouterEmploye(EB5);
+            entreprise.ajouterEmploye(EB6);
+            entreprise.ajouterEmploye(EB7);
+            // Ajout des commerciaux
+            entreprise.ajouterEmploye(com1);
+            entreprise.ajouterEmploye(com2);
+            entreprise.ajouterEmploye(com3);
+           
    
             // Affichage de l'entreprise
             System.out.println("L'entreprise comprend :" + entreprise);
