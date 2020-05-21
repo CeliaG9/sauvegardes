8dfa7034e77b17c4d9cef6f40f96e8a348fe1aa0
JulesISIS <julesc@sfr.fr>
2020-05-21 17:56:04 +0200

    Ajout de la partie sauvegarde brute

--- ProjetJava/src/projetjava/Entreprise.java
+++ ProjetJava/src/projetjava/Entreprise.java
@@ -5,15 +5,21 @@
  */
 package projetjava;
 
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.HashSet;
 
 /**
  *
  * @author mjule
  */
-public class Entreprise {
-    
+public class Entreprise implements Serializable {
+
     // Collection contenant les employés
     private final HashSet<Employe> lesEmployes;
 
@@ -21,42 +27,51 @@ public class Entreprise {
     public Entreprise() {
         lesEmployes = new HashSet<>();
     }
-    
+
     // Méthode permettant d'ajouter des employés dans la collection
-    public void ajouterEmploye(Employe employe){
+    public void ajouterEmploye(Employe employe) {
         lesEmployes.add(employe);
     }
-    
+
     // Méthode permettant de supprimer des employés de la collection
-    public void supprimerEmploye(Employe employe){
+    public void supprimerEmploye(Employe employe) {
         lesEmployes.remove(employe);
-    }    
-    
+    }
+
     // Méthode permettant de calculer les salaires de toute l'entreprise
-    public double calculSalaireEntreprise(){
+    public double calculSalaireEntreprise() {
         double salairetotal = 0;
-        for (Employe employe : lesEmployes){
+        for (Employe employe : lesEmployes) {
             salairetotal = salairetotal + employe.calculSalaire();
         }
-        return salairetotal;   
+        return salairetotal;
     }
-    
+
     // Affichage de l'entreprise (liste des employés)
     // Avec la boucle forEach // pareil que for(Employe employe : lesEmployes)
     // Affichage de toutes les informations d’un employé quel que soit son type
-    public void afficherEmployes(){
-    System.out.print("Membres de l'entreprise : \n");
-    lesEmployes.forEach((employe) -> {  
-        System.out.println(employe + " / Salaire : " + employe.calculSalaire() + " euros"); 
+    public void afficherEmployes() {
+        System.out.print("Membres de l'entreprise : \n");
+        lesEmployes.forEach((employe) -> {
+            System.out.println(employe + " / Salaire : " + employe.calculSalaire() + " euros");
         });
-    // retour à la ligne après le dernier employé de la liste de l'entreprise
-    System.out.println();
+        // retour à la ligne après le dernier employé de la liste de l'entreprise
+        System.out.println();
     }
-    
-    
- 
-    }
-    
- 
 
-    
+    public void sauver(String filePath) throws FileNotFoundException, IOException {
+        FileOutputStream fos = new FileOutputStream(filePath);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(this);
+        oos.close();
+    }
+
+    public static Entreprise lire(String filePath) throws FileNotFoundException, IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(filePath);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Entreprise obj = (Entreprise) ois.readObject();
+        ois.close();
+        return obj;
+    }
+
+}
